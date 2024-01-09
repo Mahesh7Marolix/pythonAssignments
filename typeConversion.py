d430c89f293ab3c50c1bb972fdc913a8fee6fbb7
Mahesh7Marolix <154224620+Mahesh7Marolix@users.noreply.github.com>
2024-01-10 01:20:17 +0530

    Add files via upload

---
+++ typeConversion.py
@@ -0,0 +1,16 @@
+print(int(8.0))  #converting float value to integer
+print(int(True)) #converting boolean value to integer
+print(str(True)) #converting boolean value to string
+print(float(3)) #converting integer value to float
+print(str(3.0)) #converting float value to string
+print(float(True))#converting boolean value to float
+print(int("8")) #converting string to integer
+print(float("9"))#converting string to float
+print(bool(3))#converting integer to boolean 
+print(bool(0))
+print(bool(3.0))
+'''
+Any integer or float value except 0 if  we convert them
+ to boolean  we get True as output
+'''
+print(bool("nfce"))
