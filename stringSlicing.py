21ce1474149e2a3e3ee5759b02043b5df99184b0
Mahesh7Marolix <154224620+Mahesh7Marolix@users.noreply.github.com>
2024-01-10 01:42:23 +0530

    Update stringSlicing.py

--- stringSlicing.py
+++ stringSlicing.py
@@ -4,4 +4,4 @@ print(a[:7])#if no start index is given it takes start index as 0
 print(a[7:])#if no end index is given it takes start index as length of  the given string
 print(a[:15:2])#here 2 is given so it jumps,and slices only if  the diff btw indexes equals to 2
 print(a[::-1])#here it goes in negative direction and prints the reverse of the string
-print(a[:-1])
+print(a[:-1])#negative indexing
