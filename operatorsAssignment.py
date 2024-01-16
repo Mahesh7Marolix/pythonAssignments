ad782ee8542c79ddf5cde91ee7f67f161d60f256
Mahesh7Marolix <154224620+Mahesh7Marolix@users.noreply.github.com>
2024-01-16 14:29:50 +0530

    Add files via upload

---
+++ operatorsAssignment.py
@@ -0,0 +1,20 @@
+#Airthmatic operators
+print(3+5)#addition
+print(9-7)#substraction
+print(9*3)#multiplication
+print(9%3)#prints remainder
+print(9/3)#division,prints  float value
+print(9*4-9/3+22*(33-32))#it follows bodmas rule
+print(9//3)#division,prints  int value
+print(3**2)#prints power of 3 to the power of 2
+
+#comparision 
+print(3>6)#greater than
+print(6<9)#less than
+print(5>=1)#greater than or equal
+print(9<=7)#less than or equal
+print(9==9)#equal to
+print(9!=8)#not equal
+
+#logical operators
+print(False and True )
