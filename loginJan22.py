71766fe8099f210b45fa09c9523f76f019d5f8be
Mahesh7Marolix <154224620+Mahesh7Marolix@users.noreply.github.com>
2024-01-25 16:08:07 +0530

    Add files via upload

---
+++ loginJan22.py
@@ -0,0 +1,12 @@
+userId=input("please enter your user id ")
+password=input("plese enter your password ")
+
+while (password!="Marolix@123" or userId!="Mahesh2202"):
+    if userId!="Mahesh2202":  
+        userId=input("please enter your user id ")
+        password=input("please enter  password ")
+    elif password!="Marolix@123":
+        password=input("please enter correct password ")
+print("hii Mahesh... logged in Successfully")
+    
+   
